8ce4da3d36eea0257030a7a92027dcf097f122c0
Oyedee <oyempemia@gmail.com>
2021-06-07 22:51:12 +0100

    NextThroughNotesTest implementation

---
+++ app/src/androidTest/java/com/example/notekeeper/NextThroughNotesTest.java
@@ -0,0 +1,54 @@
+package com.example.notekeeper;
+
+import junit.framework.TestCase;
+import org.junit.Rule;
+import org.junit.Test;
+
+import androidx.test.espresso.contrib.DrawerActions;
+import androidx.test.espresso.contrib.NavigationViewActions;
+import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.rule.ActivityTestRule;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.*;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
+import static androidx.test.espresso.assertion.ViewAssertions.*;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withSpinnerText;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.*;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+
+public class NextThroughNotesTest extends TestCase {
+    @Rule
+    public ActivityTestRule<NavigationMainActivity> mActivityTestRule =
+            new ActivityTestRule(NavigationMainActivity.class);
+
+    @Test
+    public void NextThroughNotes() {
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_notes));
+
+        onView(withId(R.id.list_items)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+
+        List<NoteInfo> notes = DataManager.getInstance().getNotes();
+        for(int index = 0; index < notes.size(); index++) {
+            NoteInfo note = notes.get(index);
+
+            onView(withId(R.id.spinner_courses)).check(
+                    matches(withSpinnerText(note.getCourse().getTitle())));
+            onView(withId(R.id.text_note_title)).check(matches(withText(note.getTitle())));
+            onView(withId(R.id.text_note_text)).check(matches(withText(note.getText())));
+
+            if(index < notes.size() - 1)
+                onView(allOf(withId(R.id.action_next), isEnabled())).perform(click());
+        }
+        onView(withId(R.id.action_next)).check(matches(not(isEnabled())));
+        pressBack();
+    }
+}
