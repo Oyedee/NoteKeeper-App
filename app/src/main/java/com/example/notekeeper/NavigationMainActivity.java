80b6866f9953a21bd3cf9075ea63fceb6d8734e7
Oyedee <oyempemia@gmail.com>
2021-06-08 22:46:10 +0100

    Adding drawable resource icon for Notes and courses

--- app/src/main/java/com/example/notekeeper/NavigationMainActivity.java
+++ app/src/main/java/com/example/notekeeper/NavigationMainActivity.java
@@ -8,6 +8,8 @@ import android.view.Menu;
 
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
@@ -33,6 +35,7 @@ public class NavigationMainActivity extends AppCompatActivity implements Navigat
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 
         binding = ActivityNavigationMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -47,6 +50,12 @@ public class NavigationMainActivity extends AppCompatActivity implements Navigat
         });
         //reference to our drawerLayout on the xml layout resource file / binding
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
         //reference to our navigation view ID on the xml layout resource file / binding
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
 //        // Passing each menu ID as a set of Ids because each
